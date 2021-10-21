ccb6f62a6d2b65dcd44b274bebdb309a54ea7380
valerio-pescatori <pescatorim14@gmail.com>
2021-10-21 10:16:51 +0200

    Working on LSTM currently having problems with target tensor shape

--- Python/lstm.py
+++ Python/lstm.py
@@ -1,11 +1,7 @@
-from typing import Iterator
 import torch
 import torch.nn as nn
 import json
 from pathlib import Path
-from torch.optim import optimizer
-
-from torch.serialization import normalize_storage_type
 
 # architettura nn:
 # 1  lstm layer x local mf
@@ -30,20 +26,17 @@ from torch.serialization import normalize_storage_type
 
 
 # Hyper-parameters
-
-# N.B: in fase di developing
 # 8 local features
 # 250 frames (saranno 3000 poi)
 sequence_length = 250
 input_size = 8
 hidden_size = 64  # arbitrario
-num_classes = 56  # num di animazioni
-num_epochs = 3
-batch_size = 2  # 3000
+batch_size = 56  # numero totale di animazioni
+# num_epochs = 3
 
-# forse batch_size = 3000(750)
-# e sequenze_length = 8
-# https://discuss.pytorch.org/t/batch-size-for-lstm/47619
+# target deve avere la shape [56, 250, 1] --> ho un risultato per ogni blocco (da 8) di features
+# in realtà io ho bisogno di un risultato per ogni blocco da 250*8 di features
+# quindi le features devono essere 250*8, il vettore deve essere [1, 56, 250*8] (?)
 
 
 class LSTM(nn.Module):
@@ -53,18 +46,19 @@ class LSTM(nn.Module):
         self.lstm = nn.LSTM(input_size, hidden_size, batch_first=True)
         self.dense = nn.Linear(hidden_size, 1)
 
-    def forward(self, input):
-        h_t = torch.zeros(1, input.size(0), hidden_size, dtype=torch.float)
-        c_t = torch.zeros(1, input.size(0), hidden_size, dtype=torch.float)
+    def forward(self, input):  # input deve essere un tensor[1, 250, 8]
         output = 0
-        for anim in input:
-            h_t, c_t = self.lstm(anim.view(1, anim.size(0), anim.size(1)), (h_t, c_t))
-            output = self.linear(h_t)
-        return torch.cat(output, dim=1)
+        h_t, _ = self.lstm(input)
+        output = self.dense(h_t)
+        print(output.size())
+        return output
 
 
-def preprocessData(data, input_lmf, input_gmf, target):
-    for animation in data:
+def preprocessData(file_path, input_lmf, input_gmf, target):
+    data = None
+    with open(file_path, "r") as file:
+        data = json.load(file)
+    for animation in data["Items"]:
         anim_lmf = []  # local feature per questa animazione
         for frame in animation["frames"]:
             anim_lmf.append(list(frame.values()))
@@ -74,49 +68,52 @@ def preprocessData(data, input_lmf, input_gmf, target):
 
 
 if __name__ == "__main__":
-
     # preparo training input e target
-    data = None
-    with open(str(Path.cwd().parent) + "\\training.json", "r") as file:
-        data = json.load(file)
-    # separo local features da global features
-    trainLocalFeatures, trainGlobalFeatures, trainTarget = [], [], []
-    preprocessData(data["Items"], trainLocalFeatures, trainGlobalFeatures, trainTarget)
-    trainLocalFeatures = torch.tensor(trainLocalFeatures)  # size [56, 250, 8]
-    trainGlobalFeatures = torch.tensor(trainGlobalFeatures)  # size [56, 1]
-    trainTarget = torch.tensor(trainTarget)
+    train_local_features, train_global_features, train_target = [], [], []
+    preprocessData(
+        str(Path.cwd().parent) + "\\training.json",
+        train_local_features,
+        train_global_features,
+        train_target,
+    )
+    train_local_features = torch.tensor(train_local_features)  # size [56, 250, 8]
+    train_global_features = torch.tensor(train_global_features)  # size [56, 1]
+    train_target = torch.tensor(train_target)
 
     ## preparo testing input e target
-    data = None
-    with open(str(Path.cwd().parent) + "\\testing.json", "r") as file:
-        data = json.load(file)
-    testLocalFeatures, testGlobalFeatures, testTarget = [], [], []
-    preprocessData(data["Items"], testLocalFeatures, testGlobalFeatures, testTarget)
-    testLocalFeatures = torch.tensor(testLocalFeatures)
-    testGlobalFeatures = torch.tensor(testGlobalFeatures)
-    testTarget = torch.tensor(testTarget)
+    test_local_features, test_global_features, test_target = [], [], []
+    preprocessData(
+        str(Path.cwd().parent) + "\\testing.json",
+        test_local_features,
+        test_global_features,
+        test_target,
+    )
+    test_local_features = torch.tensor(test_local_features)
+    test_global_features = torch.tensor(test_global_features)
+    test_target = torch.tensor(test_target)
 
     ## istanzio il modello
     lstm = LSTM()
     optim = torch.optim.Adam(lstm.parameters(), lr=0.001)
-    criterion = torch.nn.MSELoss()
-
-    ## mi manca il test_input e test_target
+    criterion = nn.MSELoss()
 
     ################################# TRAINING #################################
+
+    # l'input deve avere la seguente shape: [batch_size, sequence_length, input_size]
+    # quindi [56, 250, 8]
     n_steps = 10
     for step in range(n_steps):
         print("Step: ", step)
-
         optim.zero_grad()
-        print(trainLocalFeatures.size())
-        output = lstm(trainLocalFeatures)
-        loss = criterion(output, trainTarget)
+        output = lstm(train_local_features)
+        loss = criterion(
+            output, train_target
+        )  # target deve avere la stessa shape dell'input
         print("loss: ", loss.item())
         loss.backward()
         optim.step()
 
         # training completo, ora testo
         with torch.no_grad():
-            guess = lstm(testLocalFeatures)
+            guess = lstm(test_local_features)
             loss = criterion()
